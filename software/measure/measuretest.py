cf55cc90784ae9e139aff2b7bd273d5e3cd9a8b4
Nicholas Mulvenna <mulvenna@linux.com>
2016-12-26 15:57:15 -0800

    updated measuretest.py for measure module renaming

--- software/measure/measuretest.py
+++ software/measure/measuretest.py
@@ -1,11 +1,11 @@
-import Measure
+import measure
 import datetime
 
 def test_distance():
 	value = 5
 	units = "metres"
 	time = datetime.datetime.now()
-	distance = Measure.Measure(value, units, time)
+	distance = measure.Measure(value, units, time)
 	assert distance.value == value
 	assert distance.units == units
 	assert distance.time == time
