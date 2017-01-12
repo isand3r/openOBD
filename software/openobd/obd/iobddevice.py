0f004200f526881a027d08df5a30f1e867ba58aa
Kaibo Ma <you@example.com>
2017-01-12 13:10:07 -0800

    added obd read and write

---
+++ software/openobd/obd/iobddevice.py
@@ -0,0 +1,23 @@
+from measure.measure import Measure
+import abc
+
+class IOBDDevice(abc.ABC):
+
+	@abc.abstractmethod
+	def initialize(self):
+		pass
+
+	@abc.abstractproperty
+	def ready(self) -> bool:
+		pass
+
+	@abc.abstractmethod
+	def read_obd(self):
+		pass
+
+	@abc.abstractmethod
+	def send_obd(self):
+		pass
+	@abc.abstractmethod
+	def close_bus(self):
+		pass
---
+++ software/openobd/obd/obddevice.py
@@ -0,0 +1,53 @@
+from obd.iobddevice import IOBDDevice
+from measure.measure import Measure
+import can
+can.rc['interface'] = 'socketcan'
+can.rc['channel'] = 'vcan0'
+import datetime
+
+class OBDDevice(IOBDDevice):
+	"""Mock Thermometer that always gives readings with MOCK_VALUE and MOCK_UNITS"""
+
+	def __init__(self):
+		self.MOCK_VALUE = 22
+		self.MOCK_UNITS = "celsius"
+		self._ready = False
+		self.bus = None
+
+
+	def initialize(self):
+		self.bus = can.interface.Bus()
+		self._ready = True
+
+	@property
+	def ready(self) -> bool:
+		return self._ready
+
+	def read_obd(self) -> Measure:
+		"""This obd device to read CAN frames"""
+		assert self._ready
+		time = datetime.datetime.now()
+
+		try:
+			stream = self.bus.recv()
+			print("Message recieved on {}".format(self.bus.channel_info))
+			print("The Message recieved is:{}".format(stream))
+			return stream
+		except can.CanError:
+			print("Message could not be recieved")
+
+	def send_obd(self):
+		"""This obd device to send CAN frames"""
+		assert self._ready
+		time = datetime.datetime.now()
+		msg = can.Message(arbitration_id=0xc0ffee,
+			data=[0, 25, 0, 1, 3, 1, 4, 1],
+			extended_id=False)
+		try:
+			self.bus.send(msg)
+			print("Message sent on {}".format(self.bus.channel_info))
+		except can.CanError:
+			print("Message NOT sent")
+
+	def close_bus(self):
+		self.bus.shutdown()
