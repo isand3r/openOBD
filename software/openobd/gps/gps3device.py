5f428a15b35ee8473405d85f053d6ff0702fdd68
Kaibo Ma <you@example.com>
2017-01-13 18:52:33 -0800

    f

--- software/openobd/gps/gps3device.py
+++ software/openobd/gps/gps3device.py
@@ -32,16 +32,21 @@ class GPS3Device(IGPSDevice):
         for new_data in self.gps_socket:
             if(new_data):
                 self.data_stream.unpack(new_data)
-                self.longitude = float(self.data_stream.TPV['lon'])
-                self.latitude = float(self.data_stream.TPV['lat'])
-                self.altitude = float(self.data_stream.TPV['alt'])
-                return
+                self.longitude = self.data_stream.TPV['lon']
+                self.latitude = self.data_stream.TPV['lat']
+                self.altitude = self.data_stream.TPV['alt']
 
     def read_location(self) -> Location:
         """Return the same location each time"""
         self.getDataStream()
+        if(self.longitude == 'n/a'):
+            self.longitude = '0.0'
+        if(self.latitude == 'n/a'):
+            self.latitude = '0.0'
+        if(self.altitude == 'n/a'):
+            self.altitude = '0.0'
         time = datetime.datetime.now()
-        self.location = Location(self.latitude, self.longitude, self.altitude, time)
+        self.location = Location(float(self.latitude), float(self.longitude), float(self.altitude), time)
         return self.location
 
             
