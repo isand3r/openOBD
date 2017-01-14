0b28e506f320b76afad484b6acd2ff6cf21293c6
Kaibo Ma <you@example.com>
2017-01-13 19:44:13 -0800

    fixedededededededed

--- software/openobd/accelerometer/mpuacceldevice.py
+++ software/openobd/accelerometer/mpuacceldevice.py
@@ -10,10 +10,10 @@ class MPUAccelDevice(IAccelDevice):
     def __init__(self):
         self.accel  = 0
         self.gyro = 0
-        self.velocity = 0
+        self.accel_mag = 0
         self.init = None
         self._ready = False
-        self.units = 'm/s'
+        self.units = 'm/s^2'
 
     def initialize(self):
         self._ready = True
@@ -30,12 +30,12 @@ class MPUAccelDevice(IAccelDevice):
         x = self.accel['x']
         y = self.accel['y']
         z = self.accel['z']
-        self.velocity = self.calc_magn(x, y, z)
-        vector = Measure(self.velocity,self.units, time)
+        self.accel_mag = self.calc_magn(x, y, z)
+        vector = Measure(self.accel_mag,self.units, time)
         return vector
 
     def calc_magn(self, X, Y, Z):
-        """calculates the magnitude of the velocity"""
+        """calculates the magnitude of the accel"""
         assert self._ready
         accel_array = math.sqrt(X**2 + Y**2 + Z**2)
         return accel_array
