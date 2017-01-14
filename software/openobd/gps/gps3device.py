079dc8b5e5f2710c70d505c89e168bbe504c71cc
Kaibo Ma <you@example.com>
2017-01-13 18:54:29 -0800

    fdf

--- software/openobd/gps/gps3device.py
+++ software/openobd/gps/gps3device.py
@@ -40,11 +40,11 @@ class GPS3Device(IGPSDevice):
         """Return the same location each time"""
         self.getDataStream()
         if(self.longitude == 'n/a'):
-            self.longitude = '0.0'
+            self.longitude = 0.0
         if(self.latitude == 'n/a'):
-            self.latitude = '0.0'
+            self.latitude = 0.0
         if(self.altitude == 'n/a'):
-            self.altitude = '0.0'
+            self.altitude = 0.0
         time = datetime.datetime.now()
         self.location = Location(float(self.latitude), float(self.longitude), float(self.altitude), time)
         return self.location
