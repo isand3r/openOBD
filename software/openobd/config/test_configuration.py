32847cb24840e64377a964161376b53337ea35ec
Kaibo Ma <you@example.com>
2017-01-31 17:18:02 -0800

    fixed config test cases

--- software/openobd/config/test_configuration.py
+++ software/openobd/config/test_configuration.py
@@ -3,6 +3,6 @@ from ..config.configuration import Configuration
 def test_read_gps_thermo():
 	config = Configuration()
 	config.read('config/config.ini')
-	assert config.gps == 'fixed_mock'
-	assert (config.thermo == 'fixed_mock') or (config.thermo == 'rising_mock')
-	assert config.accel == 'fixed_mock'
+	assert (config.gps == 'fixed_mock') or (config.gps == 'gps3')
+	assert (config.thermo == 'fixed_mock') or (config.thermo == 'rising_mock') or (config.thermo =='mpu')
+	assert (config.accel == 'fixed_mock') or (config.accel =='mpu')
